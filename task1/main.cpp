a333018a83344ec29f67122f50220ff4d1bc454a
Mihail Dimitrov <Mihail.Dimitrov@playtech.com>
2018-08-07 18:18:09 +0300

    Some suggestions for review

--- task1/main.cpp
+++ task1/main.cpp
@@ -3,51 +3,20 @@
 
 using namespace std;
 
-
 int main()
 {
-   // Node *head, *tail;
-    //createNode(3, head, tail);
-    //createNode(4, head, tail);
-    //createNode(33, head, tail);
-    //createNode(41, head, tail);
-    //createNode(6, head, tail);
-    //createNode(7, head, tail);
-    //createNode(8, head, tail);
-    //createNode(1, head, tail);
-
-    createNode(1);
-    createNode(12);
-    createNode(3);
-    createNode(3);
-    createNode(5);
-    createNode(4);
-    createNode(1);
+    Node* list = NULL;
+    addRandomNodes(list, 10);
 
     cout << "List: ";
-    printNodes(head);
+    printList(list);
     cout << endl;
-    //printNodes(head);
-    xxx = head;  // copy
-    //printNodes(xxx);
 
-   // Node *headEven, *tailEven;
-    //Node *headOdd, *tailOdd;
+    Node* newList = NULL;
+    Rearrange(list, newList);
 
-    //Rearrange(head, headEven, tailEven, headOdd, tailOdd);
-    Rearrange();  //polzva kopieto za sysdavane na headEven and headOdd
-
-    //cout << endl;
-    //printNodes(headEven);
-
-    //cout << endl;
-    //printNodes(headOdd);
-
-    cout << endl;
-    cout << "Reorder list: ";
-    printNodes(head);
-
-    cout << endl;
+    cout << "Rearranged List: ";
+    printList(newList);
     cout << endl;
 
     return 0;
