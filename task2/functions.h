b638cf64198d45830d77d774fc2cd3aeb08900eb
Mihaela Bednikova <mihaela.bednikova@gmail.com>
2018-08-08 11:40:02 +0300

    task2

---
+++ task2/functions.h
@@ -0,0 +1,22 @@
+#include "BinTree.h"
+
+
+//proverqva dali dadeno dvoichno dyrvo e dvoichno naredeno
+bool IsBst(BinTree *tree)
+{
+    tree->isBST();
+}
+
+/*
+//proverqva dali dadeno dvoichno dyrvo e pylno
+bool IsFull()
+{
+
+}
+
+//poluchava proizvolno dvoichno dyrvo i go pravi dvoichno naredeno dyrvo
+bool ReduceToBst()
+{
+
+}
+*/
