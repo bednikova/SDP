0ca6c92aefead7ae13c3cda1ecc35814f442ebb3
Mihaela Bednikova <mihaela.bednikova@gmail.com>
2018-08-14 11:46:01 +0300

    Added function delete node

--- task2/task2-version/BinTree.h
+++ task2/task2-version/BinTree.h
@@ -44,6 +44,10 @@ class BinTree
 
         void displayTreeImpl(node* n, int);
 
+
+        node* Delete(node *root, int data);
+        int FindMin(node *root);
+
 };
 
 
@@ -317,4 +321,71 @@ bool BinTree::IsBstImpl(node* node)
     {
         return (IsBstImpl(node->left) && IsBstImpl(node->right)) ? true : false;
     }
+}
+
+
+//delete root
+
+node* BinTree::Delete(node *root, int data)
+{
+  if (root == NULL)
+  {
+      return NULL;
+  }
+  if (data < root->data)
+  {
+      // data is in the left sub tree.
+      root->left = Delete(root->left, data);
+  }
+  else if (data > root->data)
+  {
+      // data is in the right sub tree.
+      root->right = Delete(root->right, data);
+  }
+  else
+  {
+     // case 1: no children
+     if (root->left == NULL && root->right == NULL)
+     {
+        delete(root); // wipe out the memory, in C, use free function
+        root = NULL;
+     }
+     // case 2: one child (right)
+     else if (root->left == NULL)
+     {
+        node *temp = root; // save current node as a backup
+        root = root->right;
+        delete temp;
+     }
+     // case 3: one child (left)
+     else if (root->right == NULL)
+     {
+        node *temp = root; // save current node as a backup
+        root = root->left;
+        delete temp;
+     }
+     // case 4: two children
+     else
+     {
+        //node *temp = FindMin(root->right); // find minimal value of right sub tree
+        root->data = FindMin(root->right); // temp->data; // duplicate the node
+        root->right = Delete(root->right, FindMin(root->right));// temp->data); // delete the duplicate node
+     }
+  }
+  return root; // parent node can update reference
+}
+
+
+
+int BinTree::FindMin(node *root)
+{
+   if (root == NULL)
+   {
+      return NULL;//INT_MAX; // or undefined.
+   }
+   if (root->left != NULL)
+   {
+      return FindMin(root->left); // left tree is smaller
+   }
+   return root->data;
 }
