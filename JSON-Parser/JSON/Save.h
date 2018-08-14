e4b0f6afad8a641ab9400a0efef15a49093cc4e2
Mihaela Bednikova <mihaela.bednikova@gmail.com>
2018-08-14 22:14:41 +0300

    JSON

---
+++ JSON-Parser/JSON/Save.h
@@ -0,0 +1,3 @@
+//Save to file.
+//-All Object
+//-Extract from object.
---
+++ JSON-Parser/JSON/Оperations.h
@@ -0,0 +1,31 @@
+
+
+//print
+
+
+
+//Search by Key.
+
+
+
+
+//Change Object
+
+
+
+//Create object.
+
+
+
+//Delete an item.
+
+
+//Move.
+
+
+//Arrangement.
+
+
+//Save to file.
+//-All Object
+//-Extract from object.
