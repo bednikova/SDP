74118b68a5f384197e4ebf544069776e0008163e
Mihaela Bednikova <mihaela.bednikova@gmail.com>
2018-08-15 11:40:15 +0300

    Added function read JSON file

--- JSON-Parser/JSON/Reading.h
+++ JSON-Parser/JSON/Reading.h
@@ -1,1 +1,45 @@
+#include <iostream>
+#include <fstream>
 
+using namespace std;
+
+
+bool ReadAndValidationJSONFile(char* fileName)
+{
+    ifstream myFile(fileName);
+
+    if(myFile)
+    {
+        char buff[1024];
+        while(myFile.getline(buff, 1024))
+        {
+            char* row = new char[strlen(buff)+1];
+            strcpy(row, buff);
+
+            //shte podavam row na function, koqto shte go validira,
+            //ako ne e validen JSON obekt shte vryshta error
+        }
+    }
+
+    myFile.close();
+}
+
+
+void ReadJSONFileAndCreateJSONObject(char* fileName)
+{
+    ifstream myFile(fileName);
+
+    if(myFile)
+    {
+        char buff[1024];
+        while(myFile.getline(buff, 1024))
+        {
+            char* row = new char[strlen(buff)+1];
+            strcpy(row, buff);
+
+            //shte go zapisvame v strukturata, validniq veche json object
+        }
+    }
+
+    myFile.close();
+}
